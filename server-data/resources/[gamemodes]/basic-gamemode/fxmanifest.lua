045d55d8637ba383a225946be5638ffa3431635b
Happy591 <118082981+Happy591@users.noreply.github.com>
2023-03-21 18:02:21 +0100

    Maj ressources

--- server-data/resources/[gamemodes]/basic-gamemode/fxmanifest.lua
+++ server-data/resources/[gamemodes]/basic-gamemode/fxmanifest.lua
@@ -6,7 +6,7 @@ author 'Cfx.re <root@cfx.re>'
 description 'A basic freeroam gametype that uses the default spawn logic from spawnmanager.'
 repository 'https://github.com/citizenfx/cfx-server-data'
 
-resource_type 'gametype' { name = 'Roleplay' }
+resource_type 'gametype' { name = 'Freeroam' }
 
 client_script 'basic_client.lua'
 
