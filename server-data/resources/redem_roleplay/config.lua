e749d4ccaa75b1c403af60bb667eb25687930d75
Happy591 <118082981+Happy591@users.noreply.github.com>
2023-03-22 11:32:19 +0100

    Maj Core

--- server-data/resources/redem_roleplay/config.lua
+++ server-data/resources/redem_roleplay/config.lua
@@ -1,9 +1,12 @@
-Config	                                    = {}			-- N'y touchez pas, cela initialise la table Config.
+Config	                                    = {}			-- Don't touch this, this initializes the Config table.
 
-Config.Version                              = "1.0.0"		-- Définissez la version de votre serveur. Cela s'affiche sur les écrans de sélection de personnage et dans un filigrane.
-Config.AnonymousNames                       = true          -- Définir sur true pour que tous les joueurs aient des noms de cible anonymes ["Stranger (PlayerID)"]
-Config.DisableChallengeNotifications        = true          -- Définir sur vrai pour désactiver les notifications de défi (jeu de base)
-Config.DisableSpaceInWeaponWheel            = true          -- Réglez sur vrai pour désactiver l'espace dans la roue des armes en verrouillant le jeu (jeu de base)
-Config.MapCrossName                         = ""            -- Définissez le BLIP_NAME lorsqu'un joueur survole un blip inexistant sur la carte (jeu de base)
-Config.DisableLootBoxes                     = true          -- Définir sur vrai pour désactiver les boîtes de butin aléatoires (jeu de base)
-Config.DisableAimAssist                     = true          -- Définir sur vrai pour désactiver l'aide à la visée sur les contrôleurs (injuste pour les serveurs RP)
+Config.Version                              = "1.0.0"		-- Set the version of your server. This gets displayed on the character select screens and in a watermark.
+Config.AnonymousNames                       = true          -- Set to true to make all players have anonymous target names ["Stranger (PlayerID)"]
+Config.DisableChallengeNotifications        = true          -- Set to true to disable challenge notifications (base game)
+Config.DisableSpaceInWeaponWheel            = true          -- Set to true to disable space in weapon wheel soft locking the game (base game)
+Config.MapCrossName                         = ""            -- Set the BLIP_NAME when a player is hovered over a non-existant blip on the map (base game)
+Config.DisableLootBoxes                     = true          -- Set to true to disable random loot boxes (base game)
+Config.DisableAimAssist                     = true          -- Set to true to disable aim assist on controllers (unfair for RP servers)
+
+Config.StartingCash                         = 100           -- The amount of cash to start with
+Config.StartingBank                         = 0             -- The amount of bank money to start with
